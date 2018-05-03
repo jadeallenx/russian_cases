2a7c46a2a6113c219d3863d8f0389c2cff6cf325
Mark Allen <mrallen1@yahoo.com>
2018-05-03 17:25:49 -0500

    WIP: reading data and building data structures

--- cases.pl
+++ cases.pl
@@ -1,3 +1,14 @@
 use 5.014;
 
-use Path::Tiny;
+
+use Text::CSV_XS qw(csv);
+use Data::Printer;
+
+my $nouns = csv( in => "nouns_utf8.csv", encoding => "UTF-8", key => 'nom' );
+my $pronouns = csv( in => "pronouns_utf8.csv", encoding => "UTF-8", key => 'masc_nom' );
+my $adjectives = csv( in => "adjectives_utf8.csv", encoding => "UTF-8", key => 'masc_nom' );
+my $stems = csv( in => "sentence_stems_utf8.csv", encoding => "UTF-8", key => 'case' );
+
+p $stems;
+
+
